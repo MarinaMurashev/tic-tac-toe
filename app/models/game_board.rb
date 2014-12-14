7a7fc9472ab11f3e893cb9728db980180dfec2b9
Marina Murashev <marinamurashev@gmail.com>
2014-12-13 21:05:34 -0800

    adds validations for all square locations on game board

--- app/models/game_board.rb
+++ app/models/game_board.rb
@@ -1,6 +1,11 @@
 class GameBoard < ActiveRecord::Base
-  VALID_SQUARE_VALUES = ["x", "o"]
+  VALID_SQUARE_VALUES = [
+    X = "x",
+    O = "o"
+  ]
 
-  validates_inclusion_of :top_left, in: VALID_SQUARE_VALUES
+  validates_inclusion_of :top_left, :top_middle, :top_right, :middle_left,
+    :middle_middle, :middle_right, :bottom_left, :bottom_middle, :bottom_right,
+    in: VALID_SQUARE_VALUES, allow_nil: true
 
 end
--- spec/models/game_board_spec.rb
+++ spec/models/game_board_spec.rb
@@ -3,9 +3,69 @@ require 'rails_helper'
 describe GameBoard do
 
   describe "#valid" do
+    let(:non_valid_square_value) { "y" }
 
-    it "has top_left with values 'x' or 'o'" do
-      expect(build(:game_board_incomplete, top_left: "y")).to_not be_valid
+    it "has top_left with values 'x' or 'o' or nil" do
+      expect(build(:game_board_incomplete, top_left: non_valid_square_value)).to_not be_valid
+      expect(build(:game_board_incomplete, top_left: "x")).to be_valid
+      expect(build(:game_board_incomplete, top_left: "o")).to be_valid
+      expect(build(:game_board_incomplete, top_left: nil)).to be_valid
+    end
+
+    it "has top_middle with values 'x' or 'o' or nil" do
+      expect(build(:game_board_incomplete, top_middle: non_valid_square_value)).to_not be_valid
+      expect(build(:game_board_incomplete, top_middle: "x")).to be_valid
+      expect(build(:game_board_incomplete, top_middle: "o")).to be_valid
+      expect(build(:game_board_incomplete, top_middle: nil)).to be_valid
+    end
+
+    it "has top_right with values 'x' or 'o' or nil" do
+      expect(build(:game_board_incomplete, top_right: non_valid_square_value)).to_not be_valid
+      expect(build(:game_board_incomplete, top_right: "x")).to be_valid
+      expect(build(:game_board_incomplete, top_right: "o")).to be_valid
+      expect(build(:game_board_incomplete, top_right: nil)).to be_valid
+    end
+
+    it "has middle_left with values 'x' or 'o' or nil" do
+      expect(build(:game_board_incomplete, middle_left: non_valid_square_value)).to_not be_valid
+      expect(build(:game_board_incomplete, middle_left: "x")).to be_valid
+      expect(build(:game_board_incomplete, middle_left: "o")).to be_valid
+      expect(build(:game_board_incomplete, middle_left: nil)).to be_valid
+    end
+
+    it "has middle_middle with values 'x' or 'o' or nil" do
+      expect(build(:game_board_incomplete, middle_middle: non_valid_square_value)).to_not be_valid
+      expect(build(:game_board_incomplete, middle_middle: "x")).to be_valid
+      expect(build(:game_board_incomplete, middle_middle: "o")).to be_valid
+      expect(build(:game_board_incomplete, middle_middle: nil)).to be_valid
+    end
+
+    it "has middle_right with values 'x' or 'o' or nil" do
+      expect(build(:game_board_incomplete, middle_right: non_valid_square_value)).to_not be_valid
+      expect(build(:game_board_incomplete, middle_right: "x")).to be_valid
+      expect(build(:game_board_incomplete, middle_right: "o")).to be_valid
+      expect(build(:game_board_incomplete, middle_right: nil)).to be_valid
+    end
+
+    it "has bottom_left with values 'x' or 'o' or nil" do
+      expect(build(:game_board_incomplete, bottom_left: non_valid_square_value)).to_not be_valid
+      expect(build(:game_board_incomplete, bottom_left: "x")).to be_valid
+      expect(build(:game_board_incomplete, bottom_left: "o")).to be_valid
+      expect(build(:game_board_incomplete, bottom_left: nil)).to be_valid
+    end
+
+    it "has bottom_middle with values 'x' or 'o' or nil" do
+      expect(build(:game_board_incomplete, bottom_middle: non_valid_square_value)).to_not be_valid
+      expect(build(:game_board_incomplete, bottom_middle: "x")).to be_valid
+      expect(build(:game_board_incomplete, bottom_middle: "o")).to be_valid
+      expect(build(:game_board_incomplete, bottom_middle: nil)).to be_valid
+    end
+
+    it "has bottom_right with values 'x' or 'o' or nil" do
+      expect(build(:game_board_incomplete, bottom_right: non_valid_square_value)).to_not be_valid
+      expect(build(:game_board_incomplete, bottom_right: "x")).to be_valid
+      expect(build(:game_board_incomplete, bottom_right: "o")).to be_valid
+      expect(build(:game_board_incomplete, bottom_right: nil)).to be_valid
     end
   end
 end
